706b46f9fc792bca4a31f42468a3bf3cddb51965
liakos86 <liakos86@gmail.com>
2022-04-09 14:52:51 +0300

    new object for speargun

---
+++ app/src/main/java/gr/liakos/spearo/model/object/Speargun.java
@@ -0,0 +1,54 @@
+package gr.liakos.spearo.model.object;
+
+public class Speargun {
+
+    Integer gunId;
+
+    String brand;
+
+    String model;
+
+    String nickName;
+
+    Integer length;
+
+    public Integer getGunId() {
+        return gunId;
+    }
+
+    public void setGunId(Integer gunId) {
+        this.gunId = gunId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+}
